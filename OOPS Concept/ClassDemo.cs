f9cc89e115edcf10180ac166b2d256d750082fde
Dhruv Rathod <dhruvrathod3110@gmail.com>
2022-07-25 18:04:50 +0530

    Add project files.

---
+++ OOPS Concept/ClassDemo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPS_Concept
+{
+    public class ClassDemo
+    {
+        int a = 10;  //property
+        int b = 20;
+        int ans;
+
+        public void sum()  //Method
+        {
+            ans = a + b;
+        }
+
+
+    }
+
+}
---
+++ OOPS Concept/MethodTypesDemo.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPS_Concept
+{
+    class MethodTypes
+    {
+        // Get Something Retun Nothig
+        public void sum1(int a, int b)
+        {
+            int ans = a + b;
+            Console.WriteLine(ans);
+        } 
+
+        //Get Nothing Return Something
+        public int sum2()
+        {
+            int ans = 2 + 3;
+            return ans;
+
+        }
+
+        // Get Nothing Return Nothing
+        public void sum3()
+        {
+            int ans = 2 + 3;
+            Console.WriteLine(ans);
+        }
+
+        //Get Something Return Something
+        public int sum4(int a , int b)
+        {
+            int ans = a + b;
+            return ans; 
+        }
+
+    }
+
+    public class MethodTypesDemo
+    {
+        public static void Main()
+        {
+            MethodTypes mt = new MethodTypes();
+            int a = 2;
+            int b = 3;
+            mt.sum1(a, b);
+
+
+           int ans1 = mt.sum2();
+           Console.WriteLine(ans1);
+
+            mt.sum3();
+
+            int ans2 = mt.sum4(a, b);
+            Console.WriteLine(ans2);
+
+            
+
+
+        }
+    }
+}
